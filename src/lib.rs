c5dee85a73c6d305cb140866f57bf5c68ba09e46
Ian Herbig <ian.herbig@gmail.com>
2015-12-04 23:43:17 -0600

    Alter size of Memory array and add simple test for Memory.

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,17 @@ pub mod cpu;
 mod memory;
 mod flags;
 
+#[test]
+fn test_memory() {
+    let mut mem = memory::Memory::new();
+
+    for i in 1..11 {
+        mem.set_byte(i, i as u8);
+    }
+
+    for i in 1..11 {
+        assert_eq!(i, mem.get_byte(i) as usize);
+    }
+
+    assert_eq!(0, mem.get_byte(12) as usize);
+}
--- src/memory.rs
+++ src/memory.rs
@@ -3,19 +3,23 @@ use std::fs::File;
 use std::io::Read;
 
 pub struct Memory {
-	ram: [u8; 131072],
+	ram: [u8; 4096],
 }
 
 impl Memory {
-	pub fn get_byte_from_ram(&self, address: usize) -> u8 {
+    pub fn new() -> Memory {
+        Memory { ram: [0; 4096] }
+    }
+
+	pub fn get_byte(&self, address: usize) -> u8 {
 		self.ram[address]
 	}
 
-	pub fn set_ram_byte(&mut self, address: usize, value: u8) {
+	pub fn set_byte(&mut self, address: usize, value: u8) {
 		self.ram[address] = value;
 	}
 
-	pub fn get_word_from_ram(&self, address: usize) -> u16 {
+	pub fn get_word(&self, address: usize) -> u16 {
 		let lowbyte:  u8 = self.ram[address];
 		let highbyte: u8 = self.ram[address+1];
 
@@ -25,7 +29,7 @@ impl Memory {
 		result
 	}
 
-	pub fn set_ram_word(&mut self, address: usize, value: u16) {
+	pub fn set_word(&mut self, address: usize, value: u16) {
 		let lowbyte:  u8 = (value & 0x00ff) as u8;
 		let highbyte: u8 = ((value & 0xff00) >> 8) as u8;
 
